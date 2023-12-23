b3aa12057485804103c9848aff487c7cdb3d6eeb
Ian Atha <ian@atha.io>
2023-12-23 16:54:29 +0200

    ap, apeirod @ 0.1.1

--- Formula/ap.rb
+++ Formula/ap.rb
@@ -1,8 +1,8 @@
 class Ap < Formula
   desc "CLI client for apeirod, a novel ECMAScript runtime"
   homepage "https://github.com/ianatha/apeiro"
-  url "https://github.com/ianatha/apeiro/releases/download/v0.1.0/ap-v0.1.0-aarch64-apple-darwin.tar.gz"
-  sha256 "551716d408aacb7d0e22a3d35d33343068e7f08a8f818f347dd20f445557bf95"
+  url "https://github.com/ianatha/apeiro/releases/download/v0.1.1/ap-v0.1.1-aarch64-apple-darwin.tar.gz"
+  sha256 "b7e87235e481af5779f73b55d1f5001852681bffb250a2bd6c1d97f4e5fe2651"
 
   def install
     bin.install "ap"
--- Formula/apeirod.rb
+++ Formula/apeirod.rb
@@ -1,8 +1,8 @@
 class Apeirod < Formula
   desc "Novel ECMAScript runtime"
   homepage "https://github.com/ianatha/apeiro"
-  url "https://github.com/ianatha/apeiro/releases/download/v0.1.0/apeirod-v0.1.0-aarch64-apple-darwin.tar.gz"
-  sha256 "95f29e240d56e84e1426b57ba86d7aaa91ae96ddc17bc9a4572a171e4d9cd173"
+  url "https://github.com/ianatha/apeiro/releases/download/v0.1.1/apeirod-v0.1.1-aarch64-apple-darwin.tar.gz"
+  sha256 "54347488c0bdfebad3b1580ff440656d565d58bfcaba10de0e1fd75b4e203dd9"
 
   def install
     bin.install "apeirod"
